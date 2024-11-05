4653fc7881965d4e31f6a3a121abe9dcdff22329
adamsiekerski <adamsiekerski@gmail.com>
2024-11-05 07:43:26 +0100

    Adding gitignore to Rider folder

---
+++ DemoApp.Core/DependencyInjection/LoggerModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+using Serilog;
+
+namespace DemoApp.Core.DependencyInjection;
+
+public class LoggerModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterInstance(Log.Logger).As<ILogger>().SingleInstance();
+    }
+}
